d4dddab4ea7ad9dbb4eb124a4a6ecb289c709159
reoccurcat <reoccurcat@gmail.com>
2022-05-13 12:29:10 -0500

    card added

---
+++ docs/card/fix.py
@@ -0,0 +1,11 @@
+# This is to dissect the carrd website
+with open("./index.html", "r") as f:
+    file = f.read()
+while True:
+    try:
+        file = file.replace('><', '>\n<')
+    except:
+        break
+with open("./index.html", "w") as f:
+    f.write(file)
+print("done.")
